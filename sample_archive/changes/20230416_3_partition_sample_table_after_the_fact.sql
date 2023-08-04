09b871e4dd47bcf086215ae0d01c0d4988f6b469
CJ Estel <cj@cjestel.net>
2023-08-04 14:01:32 -0400

    wip

--- sample_archive/changes/20230416_3_partition_sample_table_after_the_fact.sql
+++ sample_archive/changes/20230416_3_partition_sample_table_after_the_fact.sql
@@ -25,9 +25,9 @@ alter table sample_table rename to sample_table_legacy;
 alter index sample_table_pkey rename to sample_table_legacy_pkey;
 alter index sample_table_created_on_idx rename to sample_table_legacy_created_on_idx;
 alter index sample_table_slug_address_idx rename to sample_table_legacy_slug_address_idx;
-alter table sample_table_legacy drop constraint sample_table_pkey;
+alter table sample_table_legacy drop constraint sample_table_legacy_pkey;
 alter index sample_table_future_pk rename to sample_table_legacy_future_pk;
-alter table sample_table_legacy add constraint sample_table_legacy_pkey primary key using index sample_table_future_pk;
+alter table sample_table_legacy add constraint sample_table_legacy_pkey primary key using index sample_table_future_legacy_pk;
 
 CREATE TABLE sample_table (
   id bigint NOT NULL DEFAULT nextval('sample_table_id_seq'::regclass) ,
